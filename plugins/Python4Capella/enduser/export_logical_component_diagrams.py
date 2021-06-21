3e3199d4f29443a1d4565b4e4458cf5391d3b0ba
Yvan Lussaud <yvan.lussaud@obeo.fr>
2021-06-21 12:41:45 +0200

    Added batch enduser example.

--- plugins/Python4Capella/enduser/export_logical_component_diagrams.py
+++ plugins/Python4Capella/enduser/export_logical_component_diagrams.py
@@ -1,21 +1,31 @@
-# name                 : CapellaScript Sample
+# name                 : Export Diagrams
 # script-type          : Python
-# description          : CapellaScript Sample
+# description          : Exports diagrams from the selected LogicalComponent
 # popup                : enableFor(org.polarsys.capella.core.data.la.LogicalComponent)
 
+#
+# This script contribute a menu "Export Diagrams" on LogicalComponents.
+# You can right click on a LogicalComponent to access the menu.
+#
+
+# includes needed to interact with the Eclipse platform
 loadModule('/System/Platform')
 loadModule('/System/UI')
+# include needed for the Capella simplified API
 include('workspace://Python4Capella/simplified_api/capella.py')
 if False:
     from simplified_api.capella import *
 
+# Creates a LogicalComponent from the current selection
 lc = LogicalComponent(getSelection().getFirstElement())
+
 print(lc.get_name() + "'s realizing components")
 for rc in lc.get_realizing_physical_components():
     #: :type rc: Component
     print(" - " + rc.get_name())
 print("")
 
+# Iterates over representations to export them
 for rd in lc.get_representation_descriptors():
     #: :type rd: DRepresentationDescriptor
     print("exporting " + rd.get_name())
