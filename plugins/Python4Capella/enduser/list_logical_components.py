87fc4fe62dccdc8bb7ec4c56b4efb91a20f28c53
Yvan Lussaud <yvan.lussaud@obeo.fr>
2021-06-22 10:34:30 +0200

    Added Python script with parameter.

--- plugins/Python4Capella/enduser/list_logical_components.py
+++ plugins/Python4Capella/enduser/list_logical_components.py
@@ -1,8 +1,10 @@
 #
-# This script loads the IFE Capella model and list all its LogicalComponent.
+# This script loads the Capella model passed as first argument and list all its LogicalComponent.
 # To run it:
-#  - enable Developer capabilities if not alreadey done (see documentation in the help menu)
-#  - right click this script and select the Run As / EASE Script menu
+#  - enable Developer capabilities if not already done (see documentation in the help menu)
+#  - right click this script and select the Run As / Run configuration menu
+#    - define the path to the aird file as first argument
+#      for instance: "/In-Flight Entertainment System/In-Flight Entertainment System.aird"
 #
 
 # include needed for the Capella modeller API
@@ -10,8 +12,9 @@ include('workspace://Python4Capella/simplified_api/capellamodeller.py')
 if False:
     from simplified_api.capellamodeller import *
 
-# Load the IFE example
-model = CapellaModel("/In-Flight Entertainment System/In-Flight Entertainment System.aird")
+# Load the Capella model from the first argument of the script
+aird_path = argv[0]
+model = CapellaModel(aird_path)
 
 # gets the SystemEngineering and print its name
 se = model.get_system_engineering()
