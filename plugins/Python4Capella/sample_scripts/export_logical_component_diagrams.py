77163d7d73e836bc931d86b0b4f1b9ae8e2cc371
Yvan Lussaud <yvan.lussaud@obeo.fr>
2021-07-02 11:57:29 +0200

    Added folder creation to export_logical_component_diagrams.py.

--- plugins/Python4Capella/sample_scripts/export_logical_component_diagrams.py
+++ plugins/Python4Capella/sample_scripts/export_logical_component_diagrams.py
@@ -6,11 +6,13 @@
 #
 # This script contribute a menu "Export Diagrams" on LogicalComponents.
 # You can right click on a LogicalComponent to access the menu.
-#
+# It will create a "Python4Capella_exported_diagrams" in your Capella project
+# and export diagrams to it.
 
 # includes needed to interact with the Eclipse platform
 loadModule('/System/Platform')
 loadModule('/System/UI')
+loadModule('/System/Resources')
 # include needed for the Capella simplified API
 include('workspace://Python4Capella/simplified_api/capella.py')
 if False:
@@ -19,14 +21,19 @@ if False:
 # Creates a LogicalComponent from the current selection
 lc = LogicalComponent(getSelection().getFirstElement())
 
-print(lc.get_name() + "'s realizing components")
-for rc in lc.get_realizing_physical_components():
-    #: :type rc: Component
-    print(" - " + rc.get_name())
-print("")
+# create  a folder in the project
+model_path = lc.get_java_object().eResource().getURI().toPlatformString(True);
+project_name = model_path[1:(model_path.index("/", 1) + 1)]
+project = getProject(project_name)
+folder = project.getFolder("Python4Capella_exported_diagrams")
+if not folder.exists():
+    folder.create(True, True, None)
+
 
 # Iterates over representations to export them
 for rd in lc.get_representation_descriptors():
     #: :type rd: DRepresentationDescriptor
     print("exporting " + rd.get_name())
-    rd.export_image("/tmp/" + rd.get_name() + ".jpg")
+    rd.export_image(folder.getLocation().toString() + "/" + rd.get_name() + ".jpg")
+
+folder.refreshLocal(2, None)
