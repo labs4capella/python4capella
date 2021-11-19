375b1df3729d113fa496ddcb1299e52cdbd81b74
Sophie-Pl <89489691+Sophie-Pl@users.noreply.github.com>
2021-11-19 15:43:15 +0100

    Update Need8.py

--- plugins/Python4Capella/sample_scripts/Need8.py
+++ plugins/Python4Capella/sample_scripts/Need8.py
@@ -5,10 +5,16 @@ It will create a folder result in the selected Capella project with the resultin
 '''
 # To run it:
 #  - enable Developer capabilities if not already done (see documentation in the help menu)
-#  - launch the contextual menu "Run As / Run configurations..." on this script
+#  - you can run this script by launching the contextual menu "Run As / EASE Script..." 
+#    on this script. 
+#    - By default, the model selected is IFE sample (aird path of the model written below)
+
+#  - you can also run this script according to a configuration (script selected, arguments) 
+#    and modify the configuration by launching the contextual menu "Run As / Run configurations..." 
+#    on this script. 
 #    - create a new "EASE Script" configuration
-#    - define the name of the configuration: "Need8.py" (for instance)
-#    - define the Script Source path: "workspace://Python4Capella/sample_scripts/Need8.py"
+#    - define the name of the configuration: "Export_description_of_elements _with_html format_and_as_plain text_to_xlsx.py" (for instance)
+#    - define the Script Source path: "workspace://Python4Capella/sample_scripts/Export_description_of_elements _with_html format_and_as_plain text_to_xlsx.py"
 #
 
 # include needed for the Capella modeller API
@@ -45,8 +51,22 @@ def strip_tags(html):
     s.feed(html)
     return s.get_data()
 
-# change this path to execute the script on your model
+# change this path to execute the script on your model (here is the IFE sample). 
+# comment it if you want to use the "Run configuration" instead
 aird_path = '/In-Flight Entertainment System/In-Flight Entertainment System.aird'
+
+'''
+#Here is the "Run Configuration" part to uncomment if you want to use this functionality :
+
+#check parameter numbers
+if len(argv) != 1:
+    # use IFE default values
+    aird_path = "/In-Flight Entertainment System/In-Flight Entertainment System.aird"
+else:
+    # Load the Capella model from the first argument of the script
+    aird_path = argv[0]
+'''
+
 model = CapellaModel()
 model.open(aird_path)
 
@@ -58,7 +78,7 @@ print('starting export of model ' + se.get_name())
 project_name = aird_path[0:(aird_path.index("/", 1) + 1)]
 project = CapellaPlatform.getProject(project_name)
 folder = CapellaPlatform.getFolder(project, 'results')
-xlsx_file_name = CapellaPlatform.getAbsolutePath(folder) + '/' + 'Need8.xlsx'
+xlsx_file_name = CapellaPlatform.getAbsolutePath(folder) + '/' + 'Export_description_of_elements _with_html format_and_as_plain text_to_xlsx.xlsx'
 # create a workbook
 workbook = Workbook()
 
