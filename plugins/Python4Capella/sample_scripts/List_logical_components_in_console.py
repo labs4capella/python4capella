f8cfe636ad55168f00a8132d0116d265622963aa
Sophie-Pl <89489691+Sophie-Pl@users.noreply.github.com>
2021-11-19 15:47:42 +0100

    Update and rename list_logical_components.py to List_logical_components_in_console.py

--- plugins/Python4Capella/sample_scripts/List_logical_components_in_console.py
+++ plugins/Python4Capella/sample_scripts/List_logical_components_in_console.py
@@ -2,16 +2,29 @@
 # This script loads the Capella model passed as first argument and list its root LogicalComponent.
 # To run it:
 #  - enable Developer capabilities if not already done (see documentation in the help menu)
-#  - launch the contextual menu "Run As / Run configurations..." on this script
+#  - you can run this script by launching the contextual menu "Run As / EASE Script..." 
+#    on this script. 
+#    - By default, the model selected is IFE sample (aird path of the model written below)
+
+#  - you can also run this script according to a configuration (script selected, arguments) 
+#    and modify the configuration by launching the contextual menu "Run As / Run configurations..." 
+#    on this script. 
 #    - create a new "EASE Script" configuration
 #    - define the name of the configuration: "list_logical_components.py" (for instance)
-#    - define the Script Source path: "workspace://Python4Capella/sample_scripts/list_logical_components.py"
+#    - define the Script Source path: "workspace://Python4Capella/sample_scripts/List_logical_components_in_console.py"
 #    - define the path to the aird file as first argument in "Script arguments" area: "/In-Flight Entertainment System/In-Flight Entertainment System.aird" (for instance)
 
 # include needed for the Capella modeller API
 include('workspace://Python4Capella/simplified_api/capella.py')
 if False:
     from simplified_api.capella import *
+
+# change this path to execute the script on your model (here is the IFE sample). 
+# comment it if you want to use the "Run configuration" instead
+aird_path = '/In-Flight Entertainment System/In-Flight Entertainment System.aird'
+
+'''
+#Here is the "Run Configuration" part to uncomment if you want to use this functionality :
 
 #check parameter numbers
 if len(argv) != 1:
@@ -20,6 +33,7 @@ if len(argv) != 1:
 else:
     # Load the Capella model from the first argument of the script
     aird_path = argv[0]
+'''
 
 model = CapellaModel()
 model.open(aird_path)
