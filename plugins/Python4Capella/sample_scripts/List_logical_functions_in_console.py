914964a65f93d253d9d5720c688f49f34fa5a3f0
Sophie-Pl <89489691+Sophie-Pl@users.noreply.github.com>
2021-12-06 18:31:03 +0100

    Create List_logical_functions_in_console.py

---
+++ plugins/Python4Capella/sample_scripts/List_logical_functions_in_console.py
@@ -0,0 +1,57 @@
+# name                 : List logical functions in console
+# script-type          : Python
+# description          : List logical functions in console
+# popup                : enableFor(org.polarsys.capella.core.data.capellacore.CapellaElement)
+#
+# This script loads the Capella model passed as first argument and list its root LogicalFunction.
+# To run it:
+#  - enable Developer capabilities if not already done (see documentation in the help menu)
+#  - you can run this script by launching the contextual menu "Run As / EASE Script..." 
+#    on this script. 
+#    - By default, the model selected is IFE sample (aird path of the model written below)
+
+#  - you can also run this script according to a configuration (script selected, arguments) 
+#    and modify the configuration by launching the contextual menu "Run As / Run configurations..." 
+#    on this script. 
+#    - create a new "EASE Script" configuration
+#    - define the name of the configuration: "list_logical_functions_in_console.py" (for instance)
+#    - define the Script Source path: "workspace://Python4Capella/sample_scripts/List_logical_functions_in_console.py"
+#    - define the path to the aird file as first argument in "Script arguments" area: "/In-Flight Entertainment System/In-Flight Entertainment System.aird" (for instance)
+
+# include needed for the Capella modeller API
+include('workspace://Python4Capella/simplified_api/capella.py')
+if False:
+    from simplified_api.capella import *
+
+# Retrieve the Element from the current selection and its aird model path
+selected_elem = CapellaElement(CapellaPlatform.getFirstSelectedElement())
+aird_path = '/'+ CapellaPlatform.getModelPath(selected_elem)
+
+'''
+# change this path to execute the script on your model (here is the IFE sample). 
+# Uncomment it if you want to use the "Run configuration" instead
+aird_path = '/In-Flight Entertainment System/In-Flight Entertainment System.aird'
+'''
+'''
+#Here is the "Run Configuration" part to uncomment if you want to use this functionality :
+
+#check parameter numbers
+if len(argv) != 1:
+    # use IFE default values
+    aird_path = "/In-Flight Entertainment System/In-Flight Entertainment System.aird"
+else:
+    # Load the Capella model from the first argument of the script
+    aird_path = argv[0]
+'''
+
+model = CapellaModel()
+model.open(aird_path)
+
+# gets the SystemEngineering and print its name
+se = model.get_system_engineering()
+print("The model name is " + se.get_name())
+
+# print the name of each LogicalFunction
+for lf in se.get_all_contents_by_type(LogicalFunction):
+    #: :type lf: LogicalFunction
+    print(" - " + lf.get_name())
