63a4895a5501e0a740c62d53f3a2b795a7ac7770
Yvan Lussaud <yvan.lussaud@obeo.fr>
2023-05-02 16:34:15 +0200

    Fixed #160 Add type parameter on JavaList and JavaIterator.

--- plugins/Python4Capella/java_api/Java_API.py
+++ plugins/Python4Capella/java_api/Java_API.py
@@ -1,3 +1,4 @@
+from typing import TypeVar, Iterator, Any, List
 def iteratorHasNext(iterator): # provider by org.eclipse.python4capella.modules.JavaModule
     pass
 def iteratorNext(iterator): # provider by org.eclipse.python4capella.modules.JavaModule
@@ -6,83 +7,85 @@ def iteratorNext(iterator): # provider by org.eclipse.python4capella.modules.Jav
 loadModule('/Capella/Java')
 import sys
 
+T = TypeVar("T")
+
 class JavaObject:
     """A wrapping class for a Java Object"""
-    def __init__(self, java_object):
+    def __init__(self, java_object: Any):
         self.java_object = java_object
-    def get_java_object(self):
+    def get_java_object(self) -> Any:
         """Gets the Java Object"""
         return self.java_object
-    def __eq__(self, other):
+    def __eq__(self, other: Any):
         if isinstance(other, JavaObject):
             return self.java_object == other.java_object
         else:
             return False
 
-class JavaIterator(JavaObject):
+class JavaIterator(JavaObject,Iterator[T]):
     """A wrapping class for a Java Iterator"""
-    def __init__(self, java_object, cls):
+    def __init__(self, java_object, cls: type):
         JavaObject.__init__(self, java_object)
         self.cls = cls;
         self.e_object_class = getattr(sys.modules["__main__"], "EObject")
-    def next(self):
+    def next(self) -> T:
         """Gets the next element"""
         if iteratorHasNext(self.get_java_object()):
-            next = iteratorNext(self.get_java_object())
-            specific_cls = self.e_object_class.get_class(next)
+            nxt = iteratorNext(self.get_java_object())
+            specific_cls = self.e_object_class.get_class(nxt)
             if specific_cls is not None:
-                return specific_cls(next)
+                return specific_cls(nxt)
             else:
-                return self.cls(next)
+                return self.cls(nxt)
         else:
             raise StopIteration
-    def __next__(self):
+    def __next__(self) -> T:
         return self.next()
 
-class JavaList(JavaObject):
+class JavaList(JavaObject, List[T]):
     """A wrapping class for a Java Iterator"""
-    def __init__(self, java_object, cls):
+    def __init__(self, java_object: Any, cls: type):
         JavaObject.__init__(self, java_object)
         self.cls = cls;
         self.e_object_class = getattr(sys.modules["__main__"], "EObject")
     def __iter__(self):
-        return JavaIterator(self.get_java_object().iterator(), self.cls)
-    def add(self, obj):
+        return JavaIterator[T](self.get_java_object().iterator(), self.cls)
+    def add(self, obj: T) -> bool:
         """Adds the given Object to the list"""
         return self.get_java_object().add(obj.get_java_object())
-    def add_all(self, list):
+    def add_all(self, l: List[T]) -> bool:
         """Adds all elements of the given List"""
         res = False;
-        for e in list:
+        for e in l:
             res = self.get_java_object().add(e.get_java_object()) or res
         return res
-    def remove(self, obj):
+    def remove(self, obj: Any) -> bool:
         """Removes the given Object"""
         return self.get_java_object().remove(obj.get_java_object())
-    def remove_all(self, list):
+    def remove_all(self, l: List[Any]) -> bool:
         """Removes all elements from the given List"""
         res = False;
-        for e in list:
+        for e in l:
             res = self.get_java_object().remove(e.get_java_object()) or res
         return res
-    def contains(self, obj):
+    def contains(self, obj: Any) -> bool:
         """Tells if the given Object is contained in this List"""
         return self.get_java_object().contains(obj.get_java_object())
-    def contains_all(self, obj):
+    def contains_all(self, l: List[Any]) -> bool:
         """Tells if all the elements of the given List are contained in this List"""
-        for e in list:
+        for e in l:
             if not self.get_java_object().contains(e.get_java_object()):
                 return False
         return True
-    def index_of(self, obj):
+    def index_of(self, obj: Any) -> int:
         """Gets the first index of hte given Object in this List"""
         return self.get_java_object().indexOf(obj.get_java_object())
-    def last_index_of(self, obj):
+    def last_index_of(self, obj: Any) -> int:
         """Gets the last index of hte given Object in this List"""
         return self.get_java_object().lastIndexOf(obj.get_java_object())
     def __getitem__(self, items):
         return self.get(items)
-    def get(self, index):
+    def get(self, index: int) -> T:
         """Gets the Object at the given index"""
         value = self.get_java_object().get(index)
         if value is None:
@@ -93,13 +96,13 @@ class JavaList(JavaObject):
                 return specific_cls(value)
             else:
                 return self.cls(value)
-    def clear(self):
+    def clear(self) -> bool:
         """Removes all elements from this List"""
         return self.get_java_object().clear()
-    def __len__(self):
+    def __len__(self) -> int:
         return self.size()
-    def len(self):
+    def len(self) -> int:
         return self.size()
-    def size(self):
+    def size(self) -> int:
         """Gets the size of this List"""
         return self.get_java_object().size()
